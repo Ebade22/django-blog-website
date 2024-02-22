6ae5c82421b7f28d7be2ac58778bb66cf43f9ceb
Ebade22 <raynas1982@gmail.com>
2024-02-22 15:35:34 +0100

    fix: added a database to the site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import BlogPost
 
 # Register your models here.
+class MainAppAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "published_date"]
+    list_display_links = ['id','title']
+
+admin.site.register(BlogPost, MainAppAdmin)
+
+admin.site.site_header = "Ebades Blog Panel"
+admin.site.site_title = "Admin Panel"
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length = 200, unique = True)
+    description = models.TextField()
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add = True)
+    update_at= models.DateTimeField(auto_now = True)
+    thumbnail = models.ImageField(upload_to="thumbnails", default="default.jpg")
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+
+    
